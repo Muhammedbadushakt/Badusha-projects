07fa6991dca210ac166a90170fc78c76ff364218
Muhammedbadushakt <bbadusha71@gmail.com>
2025-09-30 18:35:05 +0530

    Update user-interface.js

--- user-interface.js
+++ user-interface.js
@@ -3,6 +3,9 @@ let username,password;
 document.getElementById("submit").onclick = function(){
     username = document.getElementById("Name").value;
     password = document.getElementById("Password").value;
-    window.alert(`${username} updated successfully`)
+    if(username && password){
+        window.alert(`${username} updated successfully`)
+        window.location.href = "https://muhammedbadushakt.github.io/Rock-Papper-Scissors/"
+    }
     console.log(username,password);
 }
